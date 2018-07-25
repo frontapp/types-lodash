0ccc25c3333220e50493d71795417f98baba4a94
Quentin Calvez <quentin.calvez@quentez.com>
2018-07-25 14:58:11 -0700

    Upgrade typings

--- common/common.d.ts
+++ common/common.d.ts
@@ -3,7 +3,7 @@ import _ = require("../index");
 type GlobalPartial<T> = Partial<T>;
 declare module "../index" {
     type PartialObject<T> = GlobalPartial<T>;
-    type Many<T> = T | ReadonlyArray<T>;
+    type Many<T> = T | T[]; // TODO: Should be ReadonlyArray<T>, but requires ts2.5 to not infinitely loop
     interface LoDashStatic {
         /**
         * Creates a lodash object which wraps value to enable implicit method chain sequences.
@@ -182,7 +182,7 @@ declare module "../index" {
     type ArrayIterator<T, TResult> = (value: T, index: number, collection: T[]) => TResult;
     type ListIterator<T, TResult> = (value: T, index: number, collection: List<T>) => TResult;
     type ListIteratee<T> = ListIterator<T, NotVoid> | string | [string, any] | PartialDeep<T>;
-    type ListIterateeCustom<T, TResult> = ListIterator<T, TResult> | string | object | [string, any] | PartialDeep<T>;
+    type ListIterateeCustom<T, TResult> = ListIterator<T, TResult> | string | [string, any] | PartialDeep<T>;
     type ListIteratorTypeGuard<T, S extends T> = (value: T, index: number, collection: List<T>) => value is S;
 
     // Note: key should be string, not keyof T, because the actual object may contain extra properties that were not specified in the type.
@@ -216,6 +216,8 @@ declare module "../index" {
 
     type PropertyName = string | number | symbol;
     type PropertyPath = Many<PropertyName>;
+
+    type Omit<T, K extends keyof T> = Pick<T, ({ [P in keyof T]: P } & { [P in K]: never } & { [x: string]: never })[keyof T]>;
 
     /** Common interface between Arrays and jQuery objects */
     type List<T> = ArrayLike<T>;
